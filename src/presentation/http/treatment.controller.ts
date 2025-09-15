9043160a176db3e029afd4a372011fdf38b5e5d5
yurkenchik <yura.ilchyshyn06@gmail.com>
2025-09-15 18:05:34 +0300

    Added documentation of controller

--- src/presentation/http/treatment.controller.ts
+++ src/presentation/http/treatment.controller.ts
@@ -1,23 +1,32 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post } from "@nestjs/common";
 import { TreatmentService } from "@domain/treatment/services/treatment.service";
 import { TreatmentDomainEntity } from "@domain/treatment/entities/treatment-domain.entity";
 import { CreateTreatmentDto } from "@domain/treatment/dto/request/create-treatment.dto";
 import { UpdateTreatmentDto } from "@domain/treatment/dto/request/update-treatment.dto";
+import { ApiBody, ApiOperation, ApiParam, ApiResponse } from "@nestjs/swagger";
 
 @Controller('treatments')
 export class TreatmentController {
     constructor(private readonly treatmentService: TreatmentService) {}
 
+    @ApiOperation({ summary: 'Getting treatment by id' })
+    @ApiParam({ name: 'id', type: String })
+    @ApiResponse({ status: HttpStatus.OK, type: TreatmentDomainEntity })
     @Get(':id')
     async getTreatmentById(@Param('id') treatmentId: string): Promise<TreatmentDomainEntity> {
         return this.treatmentService.getTreatmentById(treatmentId);
     }
 
+    @ApiOperation({ summary: 'Getting all treatments' })
+    @ApiResponse({ status: HttpStatus.OK, type: [TreatmentDomainEntity] })
     @Get()
     async getTreatments(): Promise<Array<TreatmentDomainEntity>> {
         return this.treatmentService.getTreatments();
     }
 
+    @ApiOperation({ summary: 'Creating treatment' })
+    @ApiResponse({ status: HttpStatus.CREATED, type: TreatmentDomainEntity })
+    @ApiBody({ type: CreateTreatmentDto })
     @Post()
     async createTreatment(@Body() createTreatmentDto: CreateTreatmentDto): Promise<TreatmentDomainEntity> {
         return this.treatmentService.createTreatment(createTreatmentDto);
