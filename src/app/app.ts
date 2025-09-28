9985edd31a2e926b3161b1b8bc67a37b0159ff6a
yurkenchik <yura.ilchyshyn06@gmail.com>
2025-09-28 16:12:59 +0300

    Fixed launch config

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 export async function createApp() {
     const nestApplication = await NestFactory.create(AppModule);
-    await nestApplication.init();
+
+    nestApplication.useGlobalPipes(new ValidationPipe());
+    nestApplication.setGlobalPrefix('api');
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle('Hospital staff management API')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(nestApplication, swaggerConfig);
+    SwaggerModule.setup('api/documentation', nestApplication, document);
+
     return nestApplication;
 }
